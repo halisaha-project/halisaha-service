016d75723afb7be2f59081a9b1a7dc0332589492
Murat Karadeniz <murat.karadeniz@tokeninc.com>
2026-09-03 22:36:57 +0300

    feat: removed dev api

--- src/app.module.ts
+++ src/app.module.ts
@@ -24,7 +24,7 @@ import { GLOBAL_RATE_LIMIT } from './common/security/rate-limit.constants';
     }),
     ThrottlerModule.forRoot([{ name: 'default', ...GLOBAL_RATE_LIMIT }]),
     DatabaseModule,
-    MailModule.forRoot(),
+    MailModule,
     HealthModule,
     PositionsModule,
     UsersModule,
